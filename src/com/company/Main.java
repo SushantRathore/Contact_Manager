aedcab95566a3bdd69ea76794ddb6ad1f58f6f82
Sushant-Singh-Rathore <ssr7705@gmail.com>
2020-05-09 20:06:05 +0530

    Added Contact variable  and addContact in main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,38 @@ public class Main {
         // write your code here
         ContactsManager myContactManager = new ContactsManager();
 
-        Contact name ;
-        Contact phoneNumber;
+        Contact searchResult;
 
-        
+        Contact Aman =new Contact();
+
+        Aman.name="Aman";
+        Aman.email="aman420@gmail.com";
+        Aman.phoneNumber="9876543210";
+
+        Contact Suryansh =new Contact();
+
+        Suryansh.name="Suryansh";
+        Suryansh.email="suryansh2341@gmail.com";
+        Suryansh.phoneNumber="8796543210";
+
+        Contact meenakshi =new Contact();
+
+        meenakshi.name="Meenakshi";
+        meenakshi.email="menakshi2345@gmail.com";
+        meenakshi.phoneNumber="7896543210";
+
+        myContactManager.addContact(Aman);
+        myContactManager.addContact(Suryansh);
+        myContactManager.addContact(meenakshi);
+
+        searchResult =myContactManager.searchContact("Aman");
+        System.out.println(searchResult.name + " : " + searchResult.phoneNumber + "  " +searchResult.email);
+
+        searchResult =myContactManager.searchContact("Suryansh");
+        System.out.println(searchResult.name + " : " + searchResult.phoneNumber + "  " + searchResult.email);
+
+        searchResult =myContactManager.searchContact("Meenakshi");
+        System.out.println(searchResult.name + " : " + searchResult.phoneNumber +"  " + searchResult.email);
+
     }
 }
